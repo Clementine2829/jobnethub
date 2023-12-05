216cadf65afbcc2cc80bb32095ddf8dcdc58a631
Clementine <Clementinemamogale@gmail.com>
2023-12-05 09:55:33 +0200

    Created the resiter account

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,113 +1,134 @@
-import React, {Component} from "react";
-import login from "./Login.module.css"
+import React, { Component } from "react";
+import login from "./Login.module.css";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-// import HideFooter1 from ';
+import { performLogin } from "../Server/UsersFetcher";
 
-class Login extends Component{
+class Login extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props)
+    this.state = {
+      errorMessage: "",
+      username: "",
+      password: "",
+    };
+  }
 
-        this.state = ({
-            errorMessage: "",
-            username: "",
-            password: ""
-        })
+  validateUsernane = (event) => {
+    this.setState({
+      username: event.target.value,
+    });
+  };
+  validatePasswords = (event) => {
+    this.setState({
+      password: event.target.value,
+    });
+  };
+  handleSubmitLogin = async (event) => {
+    // event.preventDefault();
+
+    const username = this.state.username;
+    const password = this.state.password;
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+
+    // alert('Login successful started');
+
+    if (emailRegex.test(username) && passwordRegex.test(password)) {
+      try {
+        const response = await performLogin(username, password);
+
+        console.log("response here ", response);
+
+        // if (!response.ok) {
+        //   const errorMessage = await response.text(); // assuming the error message is in the response body
+        //   throw new Error(`Login failed: ${errorMessage}`);
+        // }
+        // alert('Login successful');
+
+        // const data = await response.json();
+        // console.log('data', data);
+        // alert('Login successful ' + data);
+        // setGlobalToken(data.token);
+        // Redirect or perform other actions after successful login
+      } catch (error) {
+        alert("Login successful error " + error);
+        console.log(error);
+        // Handle login error
+      }
+    } else {
+      this.setState({
+        errorMessage: "Invalid username or password",
+      });
     }
+  };
 
-    validateUsernane = event =>{
-        this.setState({
-            username: event.target.value
-        })
-    }
-    validatePasswords = event =>{
-        this.setState({
-            password: event.target.value
-        })
-    }
-    handleSubmitLogin = event =>{
-
-        const tempUsername = this.state.username;
-        const tempPassword = this.state.password;
-
-
-        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
-        if (emailRegex.test(tempUsername) && passwordRegex.test(tempPassword)) {
-            const userData = { tempUsername, tempPassword };
-            // send over to the server
-            alert("logged in, yey :-)") 
-        }else{
-            this.setState({
-                errorMessage: "Invaid username or password"
-            })            
-        }
-        event.preventDefault()
-    }
-
-    render(){
-        return(
-            <>
-                < Header />
-                <div className={`row`}>
-                    <div className={`col-sm-1`} ></div>
-                    <div className={`col-sm-10`} >
-                        <div className={`${login.form_container}`} >
-                            <form onSubmit={this.handleSubmitLogin}>
-                            <div className={`${login.sub_container}`}>
-                                    <span className={`${login.error_login_message}`}>
-                                        {this.state.errorMessage}
-                                    </span>
-                                </div>
-                                <div className={`${login.sub_container}`}>
-                                    <label>
-                                        <span className={`fas fa-user`}></span>
-                                        <span>Username:</span>
-                                    </label><br />
-                                    <input 
-                                        type="email" 
-                                        value={`${this.state.username}`} 
-                                        onChange={this.validateUsernane}
-                                        placeholder="Enter email address" 
-                                        required />
-                                </div>
-                                <div className={`${login.sub_container}`}>
-                                    <label>
-                                        <span className={`fas fa-lock`}></span>
-                                        <span>Password:</span>
-                                    </label><br />
-                                    <input 
-                                        type="password" 
-                                        value={`${this.state.password}`} 
-                                        onChange={this.validatePasswords}
-                                        placeholder="Enter password" 
-                                        required />
-                                </div>
-                                <div className={`${login.sub_container}`}>
-                                    <button 
-                                        className={`${login.login_submit}`} 
-                                        type="submit">
-                                            Login
-                                    </button>
-                                </div>
-                            </form>
-                            <div className={`${login.last_container}`}>
-                                <div>
-                                    <a href="#">Forgot password</a>
-                                </div>
-                                <div className={`${login.register}`}>
-                                    <a href="#">Create new account</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={`col-sm-1`} ></div>
+  render() {
+    return (
+      <>
+        <Header />
+        <div className={`row`}>
+          <div className={`col-sm-1`}></div>
+          <div className={`col-sm-10`}>
+            <div className={`${login.form_container}`}>
+              <form onSubmit={this.handleSubmitLogin}>
+                <div className={`${login.sub_container}`}>
+                  <span className={`${login.error_login_message}`}>
+                    {this.state.errorMessage}
+                  </span>
                 </div>
-                < Footer />
-            </>
-        )
-    }
+                <div className={`${login.sub_container}`}>
+                  <label>
+                    <span className={`fas fa-user`}></span>
+                    <span>Username:</span>
+                  </label>
+                  <br />
+                  <input
+                    type="email"
+                    value={this.state.username}
+                    onChange={this.validateUsernane}
+                    placeholder="Enter email address"
+                    required
+                  />
+                </div>
+                <div className={`${login.sub_container}`}>
+                  <label>
+                    <span className={`fas fa-lock`}></span>
+                    <span>Password:</span>
+                  </label>
+                  <br />
+                  <input
+                    type="password"
+                    value={this.state.password}
+                    onChange={this.validatePasswords}
+                    placeholder="Enter password"
+                    required
+                  />
+                </div>
+                <div className={`${login.sub_container}`}>
+                  <button className={`${login.login_submit}`} type="submit">
+                    Login
+                  </button>
+                </div>
+              </form>
+              <div className={`${login.last_container}`}>
+                <div>
+                  <a href="#">Forgot password</a>
+                </div>
+                <div className={`${login.register}`}>
+                  <a href="#">Create new account</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={`col-sm-1`}></div>
+        </div>
+        <Footer />
+      </>
+    );
+  }
 }
 
-export default Login
+export default Login;
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,12 +1,186 @@
-import React, {Component} from "react";
+import React, { useState } from "react";
+import registerStyles from "./Signup.module.css";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import { performRegistration } from "../Server/UsersFetcher";
 
-class Signup extends Component{
-    render(){
-        return(
-            <div>Sign up</div>
-        )
+const Register = () => {
+  const [errorMessage, setErrorMessage] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [errorFirstNameMessage, setErrorFirstNameMessage] = useState(" * ");
+  const [lastname, setLastname] = useState("");
+  const [errorLastNameMessage, setErrorLastNameMessage] = useState(" * ");
+  const [username, setUsername] = useState("");
+  const [errorUsernameMessage, setErrorUsernameMessage] = useState(" * ");
+  const [password, setPassword] = useState("");
+  const [errorPasswordMessage, setErrorPasswordMessage] = useState(" * ");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorConfirmPasswordMessage, setErrorConfirmPasswordMessage] =
+    useState(" * ");
+
+  const validateFirstname = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const validateLastname = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const validateUsername = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const validatePasswords = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const validateConfirmPassword = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleSubmitRegistration = async (event) => {
+    event.preventDefault();
+
+    // Perform additional validation if needed
+
+    if (password !== confirmPassword) {
+      setErrorMessage("Passwords do not match");
+    } else {
+      try {
+        // Call the registration API function (performRegistration)
+        const response = await performRegistration(username, password);
+
+        // Handle the registration response as needed
+        console.log("Registration response:", response);
+
+        // Redirect or perform other actions after successful registration
+      } catch (error) {
+        console.error("Registration error:", error);
+        // Handle registration error
+        setErrorMessage("Registration failed. Please try again.");
+      }
     }
+  };
 
-}
+  return (
+    <>
+      <Header />
+      <div className={`row ${registerStyles.heading}`}>
+        <h3>Create new account</h3>
+        <p>Use the form below to create a new account.</p>
+      </div>
+      <div className={`row ${registerStyles.row}`}>
+        <div className={`col-sm-1`} />
+        <div className={`col-sm-6`}>
+          <div className={`${registerStyles.form_container}`}>
+            <form onSubmit={handleSubmitRegistration}>
+              <div className={`${registerStyles.sub_container}`}>
+                <span className={`${registerStyles.error_register_message}`}>
+                  {errorMessage}
+                </span>
+              </div>
+              <div className={`${registerStyles.sub_container}`}>
+                <label>
+                  <span>Firstname:</span>
+                  <span className={`${registerStyles.error_register_message}`}>
+                    {errorFirstNameMessage}
+                  </span>
+                </label>
+                <br />
+                <input
+                  type="text"
+                  value={firstname}
+                  onChange={validateFirstname}
+                  placeholder="Enter your name"
+                  required
+                />
+              </div>
+              <div className={`${registerStyles.sub_container}`}>
+                <label>
+                  <span>Lastname:</span>
+                  <span className={`${registerStyles.error_register_message}`}>
+                    {errorLastNameMessage}
+                  </span>
+                </label>
+                <br />
+                <input
+                  type="text"
+                  value={lastname}
+                  onChange={validateLastname}
+                  placeholder="Enter your surname"
+                  required
+                />
+              </div>
+              <div className={`${registerStyles.sub_container}`}>
+                <label>
+                  <span>Username:</span>
+                  <span className={`${registerStyles.error_register_message}`}>
+                    {errorUsernameMessage}
+                  </span>
+                </label>
+                <br />
+                <input
+                  type="email"
+                  value={username}
+                  onChange={validateUsername}
+                  placeholder="Enter email address"
+                  required
+                />
+              </div>
+              <div className={`${registerStyles.sub_container}`}>
+                <label>
+                  <span>Password:</span>
+                  <span className={`${registerStyles.error_register_message}`}>
+                    {errorPasswordMessage}
+                  </span>
+                </label>
+                <br />
+                <input
+                  type="password"
+                  value={password}
+                  onChange={validatePasswords}
+                  placeholder="Enter password"
+                  required
+                />
+              </div>
+              <div className={`${registerStyles.sub_container}`}>
+                <label>
+                  <span>Confirm Password:</span>
+                  <span className={`${registerStyles.error_register_message}`}>
+                    {errorConfirmPasswordMessage}
+                  </span>
+                </label>
+                <br />
+                <input
+                  type="password"
+                  value={confirmPassword}
+                  onChange={validateConfirmPassword}
+                  placeholder="Confirm password"
+                  required
+                />
+              </div>
+              <div className={`${registerStyles.sub_container}`}>
+                <button
+                  className={`${registerStyles.register_submit}`}
+                  type="submit"
+                >
+                  Register
+                </button>
+              </div>
+            </form>
+            <div className={`${registerStyles.last_container}`}>
+              <div>
+                <a href="#">Already have an account? Login</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={`col-sm-4`}>hey hey new image</div>
+        <div className={`col-sm-1`} />
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default Signup
+export default Register;
